2144adb4dbecb38226406e15f437c1387da93000
lucky282 <66032866+lucky282@users.noreply.github.com>
2021-11-03 01:17:02 +0530

    responsive changes

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -10,7 +10,7 @@ function header() {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse text-right" id="navbarSupportedContent ">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav  mb-2 m-lg-auto">
                     <li className="nav-item">
                     <a className="nav-link " aria-current="page" href="#"><i class="fa fa-exclamation-circle"></i> Travel Alerts</a>
