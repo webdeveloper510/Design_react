d66f206d79e4ebba26d93fed7d1f6609c3c18665
sapna <developerweb6@gmail.com>
2021-11-30 00:51:58 +0530

    new design

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,7 +6,7 @@ function header() {
         <div className="container narrow">
             <nav className="navbar navbar-expand-lg navbar-light ">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#"><img src="home-alternate.png" style={{    marginTop: '-27px',marginLeft: '-20px'}} width="301px" height="70px"/></a>
+                <a className="navbar-brand" href="#"><img src="home-alternate.png" style={{    marginTop: '-27px',marginLeft: '-20px'}} width="301px" height="45px"/></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
