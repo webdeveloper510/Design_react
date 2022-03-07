068bd87b61a8655066bc5c10a0dd6c2bd33bac8d
webdeveloper510 <developerweb6@gmail.com>
2022-03-08 02:32:24 +0530

    solve image issue

--- src/Booking/StepHeader.js
+++ src/Booking/StepHeader.js
@@ -1,16 +1,18 @@
 import React from 'react';
-
+import logo from "../../public/suntours_logo.svg"
+import pro from "../../public/pro.png"
+import Locks from "../../public/Locks.png"
 function StepHeader() {
   return <div className='container'>
     <div className='row'>
       <div className='col-md-2'>
-        <img  alt='' src="suntours_logo.svg" width="277px" height="60px" className='mt-3'/>
+        <img  alt='' src={logo} width="277px" height="60px" className='mt-3'/>
       </div>
       <div className='col'></div>
       <div className='col-md-8 text-right'>
         <button className='btn btn-primary mr-3' type="button">Call to book 0800 030 5500</button>
-        <img  alt='' src="pro.png" width="84px" className=' p-3'/>
-        <img  alt='' src="Locks.png" width="46px" />
+        <img  alt='' src={pro} width="84px" className=' p-3'/>
+        <img  alt='' src={Locks} width="46px" />
       </div>
     </div>
       
